0999a237baa2ab7bfca218343b90a781fc4ac833
ruzica-glavas <ruzi99@hotmail.it>
2025-01-16 15:57:34 +0100

    add: ex.4

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -5,8 +5,28 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+    countVowels=(word)=>{
+
+        let vowels=("a,e,i,o,u,A,E,I,O,U")
+        let extractVowels=[]
+        let somma=0
+
+        for(i=0; i<word.length; i++){
+
+            if(vowels.includes(word[i])){
+                somma += vowels.includes(word[i]) ?1 :0
+                extractVowels.push(word[i])
+            }
+
+        }
+
+        return (somma, extractVowels)
+
+    }
 
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(countVowels(word))
 
 
 
